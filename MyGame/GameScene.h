3d5549e1ba690c14bd605d86762909f2e631014f
james <jamesrochabrun@gmail.com>
2016-06-29 19:02:42 -0400

    Initial Commit

---
+++ MyGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  MyGame
+//
+
+//  Copyright (c) 2016 James Rochabrun. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ MyGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  MyGame
+//
+
+//  Copyright (c) 2016 James Rochabrun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
